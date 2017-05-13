17b47d9ea3701f4af67a01b8f7a90ac185a8bfa9
Scott <sdg8979@rit.edu>
2017-05-13 18:44:35 -0400

    Created circle class

--- js/canvas.js
+++ js/canvas.js
@@ -1,8 +1,42 @@
 var game = document.getElementById("game");
 var ctx = game.getContext("2d");
+class Circle{
+    constructor(radius,x,y){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+    draw(){
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+var dx = 2; //Delta x, or change in x position
+var dy = -2;
+var ball = new Circle(10,240,160);
+function draw() {
+    ball.draw();
+}
+function drawBall(){
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+function wallCollision(){
+    if(y + dy < radius || y + dy > game.height - radius)
+    {
+        dy = -dy;
+    }
+    if(x + dx < radius || x + dx > game.width - radius)
+    {
+        dx = -dx;
+    }
+}
+setInterval(draw, 10);
