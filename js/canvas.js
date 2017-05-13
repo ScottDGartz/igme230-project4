eb9daae475754f7d9c198ac5a116fd26527c363e
Scott <sdg8979@rit.edu>
2017-05-13 19:35:04 -0400

    turned fill into a parameter for circle also created basic skeleton for rectangle class

--- js/canvas.js
+++ js/canvas.js
@@ -1,18 +1,21 @@
 var game = document.getElementById("game");
 var ctx = game.getContext("2d");
+
+//class declarations
 class Circle{
-    constructor(radius,x,y,dx,dy){
+    constructor(radius,x,y,dx,dy,fill){
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.dx = dx;
         this.dy = dy;
+        this.fill = fill;
     }
 
     draw(){
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fillStyle = "#0095DD";
+        ctx.fillStyle = this.fill;
         ctx.fill();
         ctx.closePath();
         this.wallCollision();
@@ -31,9 +34,25 @@ class Circle{
 }
 }
 
+class Rectangle{
+    constructor(){
+
+    }
+    draw(){
+        ctx.beginPath();
+        ctx.rect(this.x,this.y,this.width,this.height);
+        ctx.fillstyle= this.fill;
+        ctx.fill();
+        ctx.closePath();
+    }
+    wallCollision(){
+
+    }
+}
+
 var dx = 2; //Delta x, or change in x position
 var dy = -2;
-var ball = new Circle(10,240,160,2,2);
+var ball = new Circle(10,240,160,2,2,"#002321");
 function draw() {
     ctx.clearRect(0,0,game.width,game.height);
     ball.draw();
