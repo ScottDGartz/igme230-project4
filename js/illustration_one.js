4193a6080ecf9af1dc66213e664d5cf7c33d1b74
Scott <sdg8979@rit.edu>
2017-05-14 13:09:52 -0400

    Balls bounce off edges, rectangles wrap

--- js/illustration_one.js
+++ js/illustration_one.js
@@ -57,11 +57,17 @@ class Rectangle {
         this.loc.add(this.velo);
     }
     wallCollision() {
-        if (this.loc.x < 0 || this.loc.x + this.width > game.width) {
-            this.velo.x = -this.velo.x;
+        if(this.loc.x > game.width){
+            this.loc.x = 0;
         }
-        if (this.loc.y < 0 || this.loc.y + this.height > game.height) {
-            this.velo.y = -this.velo.y;
+        else if(this.loc.x < 0){
+            this.loc.x = game.width;
+        }
+        if(this.loc.y > game.height){
+            this.loc.y = 0;
+        }
+        else if(this.loc.y < 0){
+            this.loc.y = game.height;
         }
     }
     newTarget(b) {
@@ -75,37 +81,40 @@ class Vector {
         this.y = y;
         this.calcMag();
     }
-    subtract(b){
+    subtract(b) {
         this.x = this.x - b.x;
         this.y = this.y - b.y;
         this.calcMag();
     }
-    add(b){
+    add(b) {
         this.x = this.x + b.x;
         this.y = this.y + b.y;
         this.calcMag();
     }
-    scaleUp(n){
+    scaleUp(n) {
         this.x = this.x * n;
         this.y = this.y * n;
         this.calcMag();
     }
-    scaleDown(n){
+    scaleDown(n) {
         this.x = this.x * n;
         this.y = this.y * n;
         this.calcMag();
     }
-    calcMag(){
+    calcMag() {
         this.magnitude = Math.sqrt(this.x * this.x + this.y * this.y);
     }
-    normalize(){
-        this.scaleDown(this.magnitude);
+    normalize() {
+        if (this.magnitude != 0) {
+            this.scaleDown(this.magnitude);
+        }
     }
 }
 var dx = 2; //Delta x, or change in x position
 var dy = -2;
-var ball = new Circle(10, new Vector(240, 160), new Vector(2,2), "#D22356");
-var rect = new Rectangle(new Vector(0,0),new Vector(2,3),20,20,"#3AC3D6")
+var ball = new Circle(10, new Vector(240, 160), new Vector(2, 2), "#D22356");
+var rect = new Rectangle(new Vector(0, 0), new Vector(2, 3), 20, 20, "#3AC3D6")
+
 function draw() {
     ctx.clearRect(0, 0, game.width, game.height);
     ball.draw();
